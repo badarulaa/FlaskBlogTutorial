36ac874397d21ae232cd532de4f91229a68378bc
badarulaa <badarul93@gmail.com>
2021-09-06 11:18:13 +0700

    Finished Tutorial 5

--- website/static/index.js
+++ website/static/index.js
@@ -1,10 +1,16 @@
 function like(postId) {
   const likeCount = document.getElementById(`likes-count-${postId}`);
-  const likeButton = document.getElementById(`likes-button-${postId}`);
+  const likeButton = document.getElementById(`like-button-${postId}`);
 
-  fetch("/like-post/${postId}", { method: "POST" })
+  fetch(`/like-post/${postId}`, { method: "POST" })
     .then((res) => res.json())
-    .then((data) => console.log(data));
-
-  console.log(likeCount.value);
+    .then((data) => {
+      likeCount.innerHTML = data["likes"];
+      if (data["liked"] === true) {
+        likeButton.className = "far fa-thumbs-up";
+      } else {
+        likeButton.className = "far fa-thumbs-up";
+      }
+    })
+    .catch((e) => alert("Could not like post."));
 }
